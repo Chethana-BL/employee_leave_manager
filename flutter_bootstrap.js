c6877f9de18ac4ee7462f9d120f7db9db9579dd4
Chethana-BL <Chethana-BL@users.noreply.github.com>
2025-05-17 19:13:27 +0000

    deploy: e3f07f7ff028c0c864ec42258115116ac03c26ed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "263673202"
+    serviceWorkerVersion: "2881500516"
   }
 });
